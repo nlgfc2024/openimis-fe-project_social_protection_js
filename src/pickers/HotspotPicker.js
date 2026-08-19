25f7b3ba2c7c610708c260144ba1651600c285c1
Elvis <elvisnamuru@gmail.com>
2026-08-19 12:57:07 +0200

    Changes

--- src/pickers/HotspotPicker.js
+++ src/pickers/HotspotPicker.js
@@ -53,7 +53,7 @@ function HotspotPicker({
   const hotspots = data?.hotspots?.edges?.map((edge) => (
     {
       ...edge.node,
-      gqlId: edge.node.id, // Relay global ID: location_Id filters require the encoded form.         
+      gqlId: edge.node.id, // Relay global ID: location *_Id filters require the encoded form.
       id: decodeId(edge.node.id),
     }
   )) ?? [];
